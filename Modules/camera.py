bf1d864197679a24c069e2a6a7a94c1e6962b8fd
Tanakitti Sachati <tanakit_21235@hotmail.com>
2019-08-25 22:38:46 +0700

    add comment

--- Modules/camera.py
+++ Modules/camera.py
@@ -2,16 +2,21 @@ import cv2
 
 # initialize the camera
 cam = cv2.VideoCapture(0)
+
+# get width and height
 w = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
 
 def takePicture(filename):
+    # get picture from the webcam
     ret, image = cam.read()
     if ret:
+        # save picture
         cv2.imwrite(filename,image)
         print("Save completely"+filename+"("+str(w)+"X"+str(h)+")")
     cam.release()
 
+# test module
 if __name__ == '__main__':
     takePicture("../test.jpg")
